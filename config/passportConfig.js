6c916c715ec676c216f8402e262cb8377dcbd743
rohit <prabhoot.patel@proton.me>
2024-07-18 23:26:44 +0530

    push

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,9 +1,8 @@
-// config/passportConfig.js
 require('dotenv').config();
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('../models/User');
-const { default: axios } = require('axios');
+const axios = require('axios');
 
 passport.use(new FacebookStrategy({
   clientID: process.env.CLIENT_ID,
@@ -12,34 +11,40 @@ passport.use(new FacebookStrategy({
   profileFields: ['id', 'displayName', 'email', 'picture.type(large)'],
 },
 async (accessToken, refreshToken, profile, done) => {
-  // const url=`graph.facebook.com/me?fields=id,name,accounts{fan_count,followers_count}&access_token=${accessToken}`;
-  // const responseAPI=axios.get(url).then(
-  //   console.log(JSON.stringify(responseAPI))
-  // )
-  // console.log(`${profile._raw}`);
-  const responseDB = await User.findOne({ 'fid': profile.id });
-  console.log(`profile is ${profile._raw}`);
   try {
-    if (responseDB) {
-      console.log(`user found ${responseDB}`);
-    } else {
-      const newUser = new User();
-      newUser.fid = profile.id;
-      newUser.email = profile.emails[0].value;
-      newUser.picture = profile.photos[0].value;
-      newUser.name = profile.displayName;
-      newUser.save();
-      console.log(newUser);
+    const url = 'https://graph.facebook.com/v20.0/me';
+    const params = {
+      fields: 'id,name,accounts{fan_count,followers_count}',
+      access_token: accessToken
+    };
+    const response = await axios.get(url, { params });
+    const followersCount = response.data.accounts.data[0].followers_count;
+    profile.followersCount = followersCount;
+    console.log(`followers_count is: ${followersCount}`);
+    
+    const responseDB = await User.findOne({ 'fid': profile.id });
+    if (!responseDB) {
+      const newUser = new User({
+        fid: profile.id,
+        email: profile.emails[0].value,
+        picture: profile.photos[0].value,
+        name: profile.displayName,
+        followersCount: followersCount // Save followers_count to the user object
+      });
+      await newUser.save();
+      console.log('New user created:', newUser);
     }
+
+    return done(null, profile);
   } catch (error) {
-    console.log(`no such user found`);
+    console.error('Error in Facebook authentication:', error);
+    return done(error);
   }
-  console.log(profile);
-  return done(null, profile);
-}
-));
+}));
 
 passport.serializeUser((user, done) => done(null, user));
 passport.deserializeUser((id, done) => {
   return done(null, id);
 });
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const session = require('express-session');
 const passport = require('passport');
 const authRoutes = require('./routes/authRoutes');
 const dbConnect = require('./config/config');
+const {FCount}=require('./config/passportConfig')
 require('./config/passportConfig');
-
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -23,7 +23,7 @@ app.use('/api/v1/auth', authRoutes);
 app.get('/',(req,res)=>{
     res.render("home.ejs")
 })
-app.get('/auth/facebook',passport.authenticate('facebook',{scope:['public_profile','email']}))
+app.get('/auth/facebook',passport.authenticate('facebook',{scope:['public_profile','email','pages_show_list','pages_read_engagement','pages_manage_posts']}))
 app.get('/auth/facebook/callback',passport.authenticate('facebook',{
     successRedirect:'/profile',
     failureRedirect:'/failed'
@@ -33,7 +33,7 @@ function isLoggedIn(req,res,next){
     res.redirect('/')
 }
 app.get('/profile',isLoggedIn,(req,res)=>{
-    console.log(req.user);
+    console.log(`the request is :: ${req.user}`);
      res.render('profile.ejs',{user:req.user})
 })
 app.get('/failed',(req,res)=>{
